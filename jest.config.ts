c638b0d6cae75559830e85b1820c6fbf47c2576d
Danil Tarasov <danil.tarasov@itechart-group.com>
2021-02-25 22:00:57 +0300

    feat: improve jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+export default async (): Promise<Config.InitialOptions> => ({
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
@@ -12,4 +14,4 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'ts', 'svelte'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-}
+})
